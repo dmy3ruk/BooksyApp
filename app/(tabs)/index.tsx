4cc76d072736356032636842168cbf3fb56e4d86
dmy3ruk <alinka05alya@gmail.com>
2024-11-29 21:46:02 +0200

    add files to storage

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,153 +1,172 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Text, View, FlatList, StyleSheet, TouchableOpacity } from "react-native";
-import { ScrollView } from "react-native-gesture-handler";
-
-import Rating from "@/app/components/menu";
-import Nav from "@/app/components/navigation";
-import Header from "@/app/components/header";
 import { useFonts } from "expo-font";
+import * as DocumentPicker from 'expo-document-picker';
+import { supabase } from "@/lib/supabase";
+import uuid from 'react-native-uuid';
+import Header from "@/app/components/header";
+import {router} from "expo-router";
 
-type Book = {
-  id: string;
-  title: string;
-  author: string;
-  progress: number;
-  cover?: string;
-};
-
-const books: Book[] = [
-  { id: "1", title: "Четверте крило", author: "Ребекка Ярос", progress: 0.5, cover: "https://via.placeholder.com/100" },
-  { id: "2", title: "Назва", author: "Автор", progress: 0, cover: undefined },
-  { id: "3", title: "Назва", author: "Автор", progress: 0, cover: undefined },
-  { id: "4", title: "Назва", author: "Автор", progress: 0, cover: undefined },
-];
- const HomeScreen = () => {
-  const renderBookItem = ({ item }: { item: Book }) => (     
-     
-    <View style={styles.bookItem}>
-      <View style={styles.cover} />
-      <View style={styles.bookInfo}>
-        <Text style={styles.title}>{item.title}</Text>
-        <Text style={styles.author}>{item.author}</Text>
-        <Rating />
-        <Text style={styles.progressText}>Прогрес</Text>
-        <View style={styles.progressBarContainer}>
-          <View style={[styles.progressBar, { width: `${item.progress * 100}%` }]} />
-        </View>
-      </View>
-    </View>
-    
-    
-  );
-
-const [fontsLoaded] = useFonts({
-    CustomFont: require('../../assets/fonts/ADLaMDisplay-Regular.ttf'), 
-    TitleFont:  require('../../assets/fonts/PTSerifCaption-Regular.ttf'), 
-    AuthorFont: require('../../assets/fonts/PTSerifCaption-Italic.ttf'),
+const HomeScreen = () => {
+  const [fontsLoaded] = useFonts({
+    CustomFont: require('@/assets/fonts/ADLaMDisplay-Regular.ttf'),
+    TitleFont: require('@/assets/fonts/PTSerifCaption-Regular.ttf'),
+    AuthorFont: require('@/assets/fonts/PTSerifCaption-Italic.ttf'),
   });
 
+  const [userId, setUserId] = useState('');
+  const [books, setBooks] = useState([]);
+
+  useEffect(() => {
+    getUser();
+    getFiles();
+  }, []);
+
+  useEffect(() => {
+    console.log('Books updated:', books);  // This will run when books state changes
+  }, [books]);
+
+  const getUser = async () => {
+    try {
+      const { data: { user } } = await supabase.auth.getUser();
+      if (user != null) {
+        setUserId(user.id);
+      } else {
+        setUserId('');
+      }
+    } catch (e) {
+      console.error(e);
+    }
+  };
+
+  const uploadFiles = async() => {
+    try {
+      const result = await DocumentPicker.getDocumentAsync();
+
+      const { uri: fileUri, name: fileName, mimeType } = result;
+
+      const { error } = await supabase
+          .storage
+          .from('books')
+          .upload(`${userId}/${uuid.v4()}`, {
+            uri: fileUri,
+            name: fileName,
+            type: mimeType || 'application/epub',
+          });
+
+      if (error) {
+        console.log("ERRRRRROOOOOOOOR : "+error)
+      } else {
+        getFiles();
+      }
+    } catch (e) {
+      console.error(e);
+    }
+  };
+
+  const getFiles = async () => {
+    try {
+      const { data, error } = await supabase
+          .storage
+          .from('books')
+          .list(userId + '/', {
+            limit: 10,
+            offset: 0,
+            sortBy: { column: 'name', order: 'asc' }
+          });
+
+      if (error) {
+        console.error(error);
+      } else {
+        setBooks(prevBooks => [...prevBooks, ...data]);
+      }
+    } catch (e) {
+      console.error(e);
+    }
+  };
+
+
+  if (!fontsLoaded) {
+    return (
+        <View style={styles.container}>
+          <Text>Loading...</Text>
+        </View>
+    );
+  }
+
   return (
-    <View style={styles.container}>
-      <Header />
-        <FlatList
-        data={books}
-        renderItem={renderBookItem}
-        keyExtractor={(item) => item.id}
-        contentContainerStyle={styles.bookList}
-      />
-      {/* <Nav/> */}
-      <TouchableOpacity style={styles.fab}>
-        <Text style={styles.fabText}>+</Text>
-      </TouchableOpacity>
-    </View>
+      <View style={styles.container}>
+        <Header />
+        {books.length === 0 ? (
+            <View style={styles.emptyContainer}>
+              <View style={styles.button}>
+                <Text style={styles.emptyText}>У вас ще немає доданих книжок</Text>
+                <TouchableOpacity onPress={uploadFiles} style={styles.uploadButton}>
+                  <Text style={styles.uploadButtonText}>Upload Book</Text>
+                </TouchableOpacity>
+              </View>
+            </View>
+        ) : (
+            <FlatList
+                data={books}
+                renderItem={({ item }) => (
+                    <View style={styles.bookItem}>
+                      <Text style={styles.emptyText}>{item.name}</Text>
+                    </View>
+                )}
+                keyExtractor={(item) => item.id}
+                contentContainerStyle={styles.bookList}
+            />
+        )}
+      </View>
   );
-}
-
-
-
+};
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: "#F1F8FF",
   },
+  emptyContainer: {
+    flex: 1,
+    marginTop: 145,
+    alignItems: 'center',
+  },
+  button: {
+    width: '60%',
+  },
+  emptyText: {
+    fontSize: 24,
+    textAlign: 'center',
+    marginBottom: 27,
+  },
+  uploadButton: {
+    backgroundColor: '#811826',
+    padding: 10,
+    borderRadius: 5,
+    alignItems: 'center',
+  },
+  uploadButtonText: {
+    color: 'white',
+    fontSize: 18,
+  },
   bookList: {
-    marginTop:30,
+    marginTop: 30,
     paddingHorizontal: 30,
   },
   bookItem: {
-    flexDirection: "row",
-    marginBottom: 40,
-    padding: 2,
-    marginVertical: 5,
+    marginBottom: 20,
+    padding: 10,
+    backgroundColor: "#fff",
     borderRadius: 8,
     shadowColor: "#000",
     shadowOpacity: 0.1,
     shadowRadius: 10,
     elevation: 2,
   },
-  cover: {
-    width: 100,
-    height: 128,
-    backgroundColor: "#d0d0d0",
-    borderRadius: 4,
-  },
-  bookInfo: {
-    flex: 1,
-    marginLeft: 10,
-  },
   title: {
     fontSize: 18,
-    marginBottom: 5,
     fontFamily: "TitleFont",
-    fontWeight: "regular",
   },
-  author: {
-    fontSize: 14,
-    fontFamily: "AuthorFont",
-    color: "#666",
-  },
-  progressText: {
-    marginTop:10,
-    marginBottom:6,
-    fontFamily: "TitleFont",
-    fontSize: 12,
-    color: "#888",
-  },
-  progressBarContainer: {
-    height: 11,
-    width: 213,
-    borderColor: "black",
-    borderWidth: 1,
-    borderRadius: 10,
-    overflow: "hidden",
-  },
-  progressBar: {
-    height: "100%",
-    backgroundColor: "#8B0000",
-  },
-  fab: {
-    shadowOffset:{
-      width: 0,
-      height:4
-    },
-    shadowOpacity: 0.25, 
-    position: "absolute",
-    bottom: 80,
-    right: 4,
-    shadowColor: "black",
-    shadowRadius:8,
-    backgroundColor: "#811826",
-    width: 65,
-    height: 65,
-    borderRadius: 100,
-    justifyContent: "center",
-    alignItems: "center",
-    elevation: 5,
-  },
-  fabText: {
-    color: "white",
-    fontSize: 24,
-  }
 });
 
 export default HomeScreen;
